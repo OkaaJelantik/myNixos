58d47971f811a50b7159b875d5b1386affb2198c
Oka Jelantik <okajelantikstdy@gmail.com>
2026-07-31 16:34:24 +0800

    Setup starship, file manager, noctalia integration, sisanya gatau

--- modules/home/features/kitty.nix
+++ modules/home/features/kitty.nix
@@ -3,9 +3,15 @@
     programs.kitty = {
       enable = true;
         settings = {
-          background_opacity = "0.75";
+          background_opacity = "0.6";
 	  background_blur = "1";
 	  window_padding_width = "10";
+
+	  font_family = "JetBrainsMono Nerd Font";
+	  bold_font = "auto";
+	  italic_font = "auto";
+	  bold_italic_font = "auto";
+	  font_size = "10.0";
         };
     };
   };
