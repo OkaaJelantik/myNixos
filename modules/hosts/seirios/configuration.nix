38053453875cbb03adea8547798ce90c2204f598
Oka Jelantik <okajelantikstdy@gmail.com>
2026-08-06 15:49:37 +0800

    setting flashdisk buat musik
    :

--- modules/hosts/seirios/configuration.nix
+++ modules/hosts/seirios/configuration.nix
@@ -37,10 +37,33 @@
       };
     };
 
+<<<<<<< HEAD
     nixpkgs.config.allowUnfreePredicate = pkg:
       builtins.elem (lib.getName pkg) [ "corefonts" ];
 
     fonts.packages = with pkgs; [ corefonts ];
   
+=======
+    programs.kdeconnect.enable = true;
+    services.upower.enable = true;
+    services.udisks2.enable = true;
+
+    fileSystems."/home/lann/disk" = {
+      device = "dev/sda1";
+      fsType = "exfat";
+      options = [ "users" "nofail" "uid=1000" "gid=100" "dmask=007" "fmask=117" ];
+    };
+
+    environment.sessionVariables = {
+      QT_STYLE_OVERRIDE = "adwaita-dark";
+    };
+
+    environment.systemPackages = with pkgs; [
+      adwaita-qt
+      adwaita-qt6
+      upower
+      sshfs
+    ];
+>>>>>>> fe73437 (setting flashdisk buat musik)
   };
 }
